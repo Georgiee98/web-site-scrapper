ee5e4f182574386e21efa20b595de189b5f56d02
“Gjorgi <georgiedelev@gmail.com>
2023-05-07 17:55:07 +0200

    result.html typo

--- scrapper/views.py
+++ scrapper/views.py
@@ -3,12 +3,12 @@ from bs4 import BeautifulSoup
 import requests
 
 def srape(request):
-    page = requests.get('https://www.google.com')
+    page = requests.get('https://www.facebook.com')
     soup = BeautifulSoup(page.text, 'html.parser')
     link_adress = []
     for link in soup.find_all('a'):
         link_adress.append(link.get('href'))
-    return render(request, '/scrapper/index.html', {'page': link_adress})
+    return render(request, 'scrapper/result.html', {'link_adress': link_adress})
 
 
 
