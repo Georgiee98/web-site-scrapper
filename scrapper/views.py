638b5f217caddc2a8ec7c850ef5f7190c2e0a51c
“Gjorgi <georgiedelev@gmail.com>
2023-05-07 20:07:57 +0200

    scrape functionality

--- scrapper/views.py
+++ scrapper/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from bs4 import BeautifulSoup
 import requests
 from .models import Link
 
 def srape(request):
-    page = requests.get('https://www.google.com')
-    soup = BeautifulSoup(page.text, 'html.parser')
+    if request.method == "POST":
+        site = request.POST.get('site', "")
 
-    for link in soup.find_all('a'):
-        link_adress = link.get('href')
-        link_text = link.string
-        Link.objects.create(adress=link_adress, name=link_text)
+        page = requests.get(site)
+        soup = BeautifulSoup(page.text, 'html.parser')
 
-    data = Link.objects.all()
+        for link in soup.find_all('a'):
+            link_adress = link.get('href')
+            link_text = link.string
+            Link.objects.create(adress=link_adress, name=link_text)
+        return HttpResponseRedirect('/')
+    else:
+        data = Link.objects.all()
+
     return render(request, 'scrapper/result.html', {'data': data})
