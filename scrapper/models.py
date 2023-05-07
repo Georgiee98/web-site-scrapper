ced2fb1a8c79312d7871fe5b2031516449d3736e
“Gjorgi <georgiedelev@gmail.com>
2023-05-07 18:33:06 +0200

    models typo __str__

--- scrapper/models.py
+++ scrapper/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Link(models.Model):
+
+
+
+    adress = models.CharField(max_length=255, null=True, blank=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.id} {self.name}"
--- scrapper/views.py
+++ scrapper/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 import requests
+from .models import Link
 
 def srape(request):
-    page = requests.get('https://www.facebook.com')
+    page = requests.get('https://www.google.com')
     soup = BeautifulSoup(page.text, 'html.parser')
-    link_adress = []
+
     for link in soup.find_all('a'):
-        link_adress.append(link.get('href'))
-    return render(request, 'scrapper/result.html', {'link_adress': link_adress})
+        link_adress = link.get('href')
+        link_text = link.string
+        Link.objects.create(adress=link_adress, name=link_text)
 
-
-
+    data = Link.objects.all()
+    return render(request, 'scrapper/result.html', {'data': data})
