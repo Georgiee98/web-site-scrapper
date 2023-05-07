e909acbc92a4222c1d2739c542675e8819f2ab23
“Gjorgi <georgiedelev@gmail.com>
2023-05-07 17:51:43 +0200

    scrape functionality

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from scrapper import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.srape, name='scrape'),
+
 ]
--- scrapper/views.py
+++ scrapper/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
 
-# Create your views here.
+def srape(request):
+    page = requests.get('https://www.google.com')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    link_adress = []
+    for link in soup.find_all('a'):
+        link_adress.append(link.get('href'))
+    return render(request, '/scrapper/index.html', {'page': link_adress})
+
+
+
